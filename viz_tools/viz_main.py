d34dc27d67894edddf9e42ccb69ea2cab6bbce28
Adeesh Kolluru <kolluru.adeesh@gmail.com>
2021-05-11 10:38:47 +0000

    updated all files with more tasks

--- viz_tools/viz_main.py
+++ viz_tools/viz_main.py
@@ -3,6 +3,7 @@ import random
 import sys
 import numpy as np
 import torch
+import argparse
 from ase.io import read
 from torch.utils.data import DataLoader
 from torch_geometric.data import Data
@@ -20,37 +21,68 @@ from ocpmodels.preprocessing import AtomsToGraphs
 import yaml
 from viz_utils import *
 
-yaml_link = "sample_config.yml"
+def main(args):
+    with open(args.config_yml) as f:
+        yml_file = yaml.safe_load(f)
 
-with open(yaml_link) as f:
-    yml_file = yaml.safe_load(f)
+    model_name = yml_file["model"].pop('name', None)
+    print(f"#### Loading model: {model_name}")
 
-model_name = yml_file["model"].pop('name', None)
+    checkpoint_path = yml_file['checkpoint']['src']
+    checkpoint = modify_checkpoint(checkpoint_path)
 
-torch.manual_seed(0)
-checkpoint_path = yml_file['checkpoint']['src']
-checkpoint = torch.load(checkpoint_path)
+    model = DimeNetPlusPlusWrap(**yml_file["model"])
+    model.load_state_dict(checkpoint)
+    model = OCPDataParallel(model, output_device=0, num_gpus=1)
 
-new_dict = {}
-for k, v in checkpoint["state_dict"].items():
-    name = k[14:]
-    new_dict[name] = v
+    if yml_file['dataset']['src']:
+        batch = lmdb_to_batch(yml_file['dataset']['src'])
+    else:
+        atoms = ase.io.read(
+                "../tests/models/atoms.json",
+                index=0,
+                format="json",
+        )
+        a2g = AtomsToGraphs(
+                max_neigh=12,
+                radius=6,
+                dummy_distance=7,
+                dummy_index=-1,
+                r_energy=True,
+                r_forces=True,
+                r_distances=True,
+        )
+        batch = Batch.from_data_list(a2g.convert_all([atoms]))
 
-model = DimeNetPlusPlusWrap(**yml_file["model"])
-model.load_state_dict(new_dict)
-model = OCPDataParallel(model, output_device=0, num_gpus=1)
-dataset = SinglePointLmdbDataset(yml_file['dataset'])
-collater = ParallelCollater(1)
-loader = DataLoader(
-    dataset,
-    batch_size=1,
-    shuffle=True,
-    pin_memory=True,
-    num_workers=8,
-    collate_fn=collater,
+    output = model(batch)
+    viz = model_viz(checkpoint_path)
+
+    if yml_file["task"]["computation_graph"]:
+        print("#### Plotting computation graph")
+        viz.computation_graph(model, batch)
+    if yml_file["task"]["t-sne_viz"]:
+        print("#### Plotting t-sne")
+        emb_weight = checkpoint["emb.emb.weight"].cpu().numpy()
+        viz.tsne_viz_emb(emb_weight)
+    if yml_file["task"]["pca_t-sne_viz"]:
+        print("#### Plotting PCA reduced t-sne")
+        emb_weight = checkpoint["emb.emb.weight"].cpu().numpy()
+        res = viz.pca(emb_weight, n=50)
+        viz.tsne_viz_emb(res)
+    if yml_file["task"]["raw_weights"]:
+        print("#### Plotting raw emb weights")
+        emb_weight = checkpoint["emb.emb.weight"].cpu().numpy()
+        viz.raw_weights_viz(emb_weight)
+    if yml_file["task"]["is2rs_plot"]:
+        print("#### Plotting is2rs comparison")
+        viz.create_is2rs_plots(batch, output)
+
+if __name__ == "__main__":
+    torch.manual_seed(0)
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--config-yml",
+        help="Path to config file",
     )
-data = dataset[5]
-batch = collater([data])
-output = model(batch)
-create_is2rs_plots(batch[0], output[1])
-
+    args = parser.parse_args()
+    main(args)
--- viz_tools/viz_utils.py
+++ viz_tools/viz_utils.py
@@ -6,7 +6,33 @@ import matplotlib.pyplot as plt
 import torch
 from ase import Atoms
 from ase.visualize.plot import plot_atoms
+import datetime
+import os
+import sys
+import json
+import glob
+import yaml
+import math
+import numpy as np
+import re
+from tqdm import tqdm
+from mpl_toolkits.mplot3d import Axes3D
 
+import seaborn as sns
+import pandas as pd
+
+import ase
+from ase.io.trajectory import Trajectory
+import pickle
+import torch
+
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
+from torchviz import make_dot
+from torch.utils.data import DataLoader
+
+from ocpmodels.datasets import SinglePointLmdbDataset
+from ocpmodels.common.data_parallel import OCPDataParallel, ParallelCollater
 matplotlib.use("Agg")
 
 
@@ -24,31 +50,93 @@ params = {
 }
 matplotlib.rcParams.update(params)
 
+class model_viz:
+    def __init__(self, checkpoint_path, viz_path=os.getcwd()):
+        self.viz_path = os.path.join(viz_path, "visuals")
+        self.checkpoint_path = checkpoint_path
 
-def create_is2rs_plots(batch, output, results_dir=os.getcwd()):
-    viz_path = os.path.join(results_dir, "visuals")
-    os.makedirs(viz_path, exist_ok=True)
-    atom_sets = batch_to_atoms(batch, output)
+    def computation_graph(self, model, data):
+        out = model(data)
+        dot = make_dot(out, params=dict(list(model.named_parameters()) + [("pos", data.pos)]))
+        dot.format = "png"
+        path = os.path.join(self.viz_path, "computation_graph")
+        os.makedirs(path, exist_ok=True)
+        dot.render(path)
 
-    for idx, system in enumerate(atom_sets):
-        randomid = batch.sid[idx]
-        fig, ax = plt.subplots(1, 3)
-        labels = ["initial", "predicted", "relaxed"]
-        for i in range(3):
-            ax[i].axis("off")
-            ax[i].set_title(labels[i])
+    def tsne_viz_emb(self, emb):
+        tsne = TSNE(n_components=2, verbose=1, perplexity=5, n_iter=300)
+        res = tsne.fit_transform(emb)
+        df = pd.DataFrame()
+        df["y"] = np.arange(1, 96)
+        df["tsne-dim-1"] = res[:, 0]
+        df["tsne-dim-2"] = res[:, 1]
+        plt.figure(figsize=(16,10))
+        sns_plot = sns.scatterplot(
+            x="tsne-dim-1", y="tsne-dim-2",
+            hue="y",
+            palette=sns.color_palette("hls", 95),
+            data=df,
+            legend="full",
+            alpha=0.3
+        )
+        plt.savefig(os.path.join(self.viz_path, "tsne_emb.png"))
 
-        ase.visualize.plot.plot_atoms(
-            system[0], ax[0], radii=0.8, rotation=("-75x, 45y, 10z")
-        )
-        ase.visualize.plot.plot_atoms(
-            system[1], ax[1], radii=0.8, rotation=("-75x, 45y, 10z")
-        )
-        ase.visualize.plot.plot_atoms(
-            system[2], ax[2], radii=0.8, rotation=("-75x, 45y, 10z")
-        )
-        fig.tight_layout()
-        fig.savefig(f"{viz_path}/{randomid}_is2rs.png")
+    def pca(self, emb_weight, n):
+        pca = PCA(n_components=n)
+        res = pca.fit_transform(emb_weight)
+        return res
+
+    def raw_weights_viz(self, emb_weight):
+        f, (ax1, ax2) = plt.subplots(1,2,sharey=True)
+        g1 = sns.heatmap(emb_weight.T, cmap="Reds", yticklabels=False, xticklabels=5, cbar=False, ax=ax1)
+        g2 = sns.heatmap(-1.5 + 3.0 * np.random.rand(512, 95), cmap="Reds", yticklabels=False, xticklabels=5, ax=ax2)
+        f.savefig(os.path.join(self.viz_path, "raw_emb_weights.png"))
+
+    def create_is2rs_plots(self, batch, output):
+        atom_sets = batch_to_atoms(batch[0], output)
+
+        for idx, system in enumerate(atom_sets):
+            randomid = batch[0].sid[idx]
+            fig, ax = plt.subplots(1, 3)
+            labels = ["initial", "predicted", "relaxed"]
+            for i in range(3):
+                ax[i].axis("off")
+                ax[i].set_title(labels[i])
+
+            ase.visualize.plot.plot_atoms(
+                system[0], ax[0], radii=0.8, rotation=("-75x, 45y, 10z")
+            )
+            ase.visualize.plot.plot_atoms(
+                system[1], ax[1], radii=0.8, rotation=("-75x, 45y, 10z")
+            )
+            ase.visualize.plot.plot_atoms(
+                system[2], ax[2], radii=0.8, rotation=("-75x, 45y, 10z")
+            )
+            fig.tight_layout()
+            fig.savefig(f"{self.viz_path}/{randomid}_is2rs.png")
+
+def modify_checkpoint(checkpoint_path):
+    checkpoint = torch.load(checkpoint_path)
+    new_dict = {}
+    for k, v in checkpoint["state_dict"].items():
+        name = k[14:]
+        new_dict[name] = v
+    return new_dict
+
+def lmdb_to_batch(path):
+    dataset = SinglePointLmdbDataset({'src': path})
+    collater = ParallelCollater(1)
+    loader = DataLoader(
+        dataset,
+        batch_size=1,
+        shuffle=True,
+        pin_memory=True,
+        num_workers=8,
+        collate_fn=collater,
+    )
+    data = dataset[0]
+    batch = collater([data])
+    return batch
 
 def batch_to_atoms(batch, output):
     n_systems = batch.sid.shape[0]
